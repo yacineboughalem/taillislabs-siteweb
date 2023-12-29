3affb2e1e6e071f99e3c8c690d7d13cb22fe54a5
yacineboughalem <yacin.boughalem@gmail.com>
2023-12-29 11:49:43 +0100

    change font size

--- resources/views/admin/layouts/globaljs.blade.php
+++ resources/views/admin/layouts/globaljs.blade.php
@@ -24,18 +24,18 @@
         }
     });
 
-    tinymce.init({
-        height: '400',
-        selector: '#pc-tinymce',
-        content_style: 'body { font-family: "Inter", sans-serif; }',
-        menubar: false,
-        toolbar: [
-            'styleselect fontselect fontsizeselect',
-            'undo redo | cut copy paste | bold italic | link image | alignleft aligncenter alignright alignjustify',
-            'bullist numlist | outdent indent | blockquote subscript superscript | advlist | autolink | lists charmap | print preview |  code'
-        ],
-        plugins: 'advlist autolink link image lists charmap print preview code'
-    });
+    // tinymce.init({
+    //     height: '400',
+    //     selector: '#pc-tinymce',
+    //     content_style: 'body { font-family: "Inter", sans-serif; }',
+    //     menubar: false,
+    //     toolbar: [
+    //         'styleselect fontselect fontsizeselect',
+    //         'undo redo | cut copy paste | bold italic | link image | alignleft aligncenter alignright alignjustify',
+    //         'bullist numlist | outdent indent | blockquote subscript superscript | advlist | autolink | lists charmap | print preview |  code'
+    //     ],
+    //     plugins: 'advlist autolink link image lists charmap print preview code'
+    // });
 
 
 
